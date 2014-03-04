76386098be6b1fe5e4b1481a0697321c8681ef39
Wojtek Siudzinski <admin@suda.pl>
2014-03-04 16:44:08 +0100

    AdHocCommand: updated command description

--- src/settingsadhoccommand.cpp
+++ src/settingsadhoccommand.cpp
@@ -53,7 +53,7 @@ SettingsAdHocCommand::SettingsAdHocCommand(Component *component, UserManager *us
 
 	field = Swift::BooleanFormField::create(CONFIG_STRING_DEFAULTED(component->getConfig(), "settings.xa_when_offline", "0") == "1");
 	field->setName("xa_when_offline");
-	field->setLabel("Set XA status when offline on XMPP");
+	field->setLabel("Set XA status when offline on XMPP (requires 'Stay connected')");
 	addFormField(field);
 }
 
